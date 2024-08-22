73af552d5c4be6e9aa087da7839c8c25082bbab5
rohitdhadambe <rohitdhadambe@gmail.com>
2024-08-22 20:21:10 +0530

    Fetching API

--- logoClone/src/components/header/Header.jsx
+++ logoClone/src/components/header/Header.jsx
@@ -33,7 +33,7 @@ export default function Header() {
                     >
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
-                                <NavLink
+                                <NavLink to="/"
                                     className={(isactive) =>
                                         `block py-2 pr-4 pl-3 duration-200 border-b
                                         ${isactive ? "text-orange-600" : "text-gray-600"}
@@ -43,6 +43,43 @@ export default function Header() {
                                     Home
                                 </NavLink>
                             </li>
+
+                            <li>
+                                <NavLink to="/about"
+                                    className={(isactive) =>
+                                        `block py-2 pr-4 pl-3 duration-200 border-b
+                                        ${isactive ? "text-orange-600" : "text-gray-600"}
+                                         border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                    }
+                                >
+                                   About
+                                </NavLink>
+                            </li>
+
+                            <li>
+                                <NavLink to="/Contact"
+                                    className={(isactive) =>
+                                        `block py-2 pr-4 pl-3 duration-200 border-b
+                                        ${isactive ? "text-orange-600" : "text-gray-600"}
+                                         border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                    }
+                                >
+                                    Contact
+                                </NavLink>
+                            </li>
+
+                            <li>
+                                <NavLink to="/Github"
+                                    className={(isactive) =>
+                                        `block py-2 pr-4 pl-3 duration-200 border-b
+                                        ${isactive ? "text-orange-600" : "text-gray-600"}
+                                         border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                    }
+                                >
+                                   GitHub
+                                </NavLink>
+                            </li>
+                            
                             
                             
                         </ul>
--- logoClone/src/main.jsx
+++ logoClone/src/main.jsx
@@ -2,27 +2,49 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';  // Assuming App component is being used elsewhere
 import './index.css';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Route, RouterProvider, createBrowserRouter ,createRoutesFromElements } from 'react-router-dom';
 import Layout from './Layout.jsx';
 import Home from './components/home/Home.jsx';
 import About from './components/about/about.jsx';  // Import the About component
+import Contact from './components/contact/Contact.jsx';
+import User from './components/user/User.jsx';
+import Github from './components/github/Github.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        path: '',
-        element: <Home />,
-      },
-      {
-        path: 'About',
-        element: <About />,  // Capitalized and imported
-      },
-    ]
-  }
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: '',
+//         element: <Home />,
+//       },
+//       {
+//         path: 'about',
+//         element: <About />,  // Capitalized and imported
+//       },
+//       {
+//         path: 'contact',
+//         element: <Contact />,  // Capitalized and imported
+//       },
+//     ]
+//   }
+// ]);
+
+//  OR U CAN DO BELOW 
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='user/:userid' element={<User />} />
+      <Route path='github' element={<Github />} />
+    </Route>
+  )
+)
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
