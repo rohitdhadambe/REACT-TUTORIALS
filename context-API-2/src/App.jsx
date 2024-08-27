556dab3b923362c8ca7adb1bde6604b7e9b3a094
rohitdhadambe <rohitdhadambe@gmail.com>
2024-08-27 09:52:25 +0530

    last touch

--- context-API-2/src/App.jsx
+++ context-API-2/src/App.jsx
@@ -31,13 +31,13 @@ function App() {
   useEffect(() => {
     const todos = JSON.parse(localStorage.getItem("todos"))
 
-    if (todos && todos.length > 0) {
+    if (todos && todos.length > 0) {                    //retriving info from local storage
       setTodos(todos)
     }
   }, [])
 
   useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos))
+    localStorage.setItem("todos", JSON.stringify(todos))       //setting info from local storage
   }, [todos])
   
 
@@ -46,9 +46,9 @@ function App() {
   return (
     <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
       <div className="bg-[#172842] min-h-screen py-8">
-                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
-                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
-                    <div className="mb-4">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white bg-slate-500">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2 text-black">Manage Your Todos</h1>
+                    <div className="mb-4 ">
                         {/* Todo form goes here */} 
                         <TodoForm />
                     </div>
