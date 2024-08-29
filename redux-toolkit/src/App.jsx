b805bde92305f5b3698292f77ecab37e2113bf0d
rohitdhadambe <rohitdhadambe@gmail.com>
2024-08-29 09:58:01 +0530

    redux toolkit competed

--- redux-toolkit/src/App.jsx
+++ redux-toolkit/src/App.jsx
@@ -1,12 +1,15 @@
+import AddTodo from "./components/AddTodo"
+import Todos from "./components/Tods"
 
-import './App.css'
+
 
 function App() {
  
 
   return (
     <>
-     
+     <AddTodo/>
+     <Todos/>
     </>
   )
 }
