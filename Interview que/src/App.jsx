559a5aeafb7c0244050cf366e0bf6f42656bedd1
rohitdhadambe <rohitdhadambe@gmail.com>
2024-09-14 14:11:04 +0530

    Interview quetion

---
+++ Interview que/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [value , setvalue] = useState(0)
+  const [mpvalue , setmpvalue] = useState(1)
+
+const mpfun = ()=> {
+    setmpvalue(value*5)
+    setvalue(value+1)
+}
+
+  return (
+    <>
+    <h1>main value : {value} </h1>
+    <h2>Updated value :{mpvalue} </h2>
+    <button onClick={mpfun}>Multiply by 5</button>
+    </>
+  )
+}
+
+export default App
