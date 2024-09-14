9771953194de98f8416f6f8c7a188da66df07ecc
rohitdhadambe <rohitdhadambe@gmail.com>
2024-09-14 14:29:50 +0530

    Interview quetion

--- Interview que/src/App.jsx
+++ Interview que/src/App.jsx
@@ -2,19 +2,26 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [value , setvalue] = useState(0)
-  const [mpvalue , setmpvalue] = useState(1)
+//   const [value , setvalue] = useState(2)
+//   const [mpvalue , setmpvalue] = useState(1)
 
-const mpfun = ()=> {
-    setmpvalue(value*5)
-    setvalue(value+1)
-}
+// const mpfun = ()=> {
+//     setmpvalue(value*5)
+//     setvalue(value+1)
+// }
+// ----------------------------------------------------------------------------------------------------------------------------------------------
+
+
 
   return (
     <>
-    <h1>main value : {value} </h1>
+    {/* <h1>main value : {value-1} </h1>
     <h2>Updated value :{mpvalue} </h2>
-    <button onClick={mpfun}>Multiply by 5</button>
+    <button onClick={mpfun}>Multiply by 5</button> */}
+{/* // ---------------------------------------------------------------------------------------------------------------------------------------------- */}
+
+
+
     </>
   )
 }
